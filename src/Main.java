200e3807a374bee93d51042ad0dada0f1a45a261
And-Rom <you@example.com>
2023-02-24 10:32:40 +0300

    Added task HW_5

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,46 @@
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Java HW4. Task 1 - Check order of brackets");
+
+        /*
+        HW5 Task 1
+        Написать метод, определяющую правильность расстановки скобок в выражении.
+        Могут содержаться следующие скобки: ()[]{}<>
+        () -> true
+        []{}((<>)) -> true
+        ) -> false
+        [) -> false
+        []{}(<)> -> false
+        */
+
+        Scanner in = new Scanner(System.in);
+        System.out.print("> ");
+        String brackets = in.next();
+        System.out.println(isCorrectParentheses(brackets));
+        in.close();
+
     }
+
+    public static boolean isCorrectParentheses(String parentheses) {
+        Map<Character, Character> bracket = new HashMap<>();
+        bracket.put(')', '(');
+        bracket.put('}', '{');
+        bracket.put(']', '[');
+        bracket.put('>', '<');
+        Deque<Character> checkList = new LinkedList<>();
+        char[] charsParentheses = parentheses.toCharArray();
+        for (char c : charsParentheses) {
+            if (bracket.containsValue(c)) {
+                checkList.push(c);
+            } else if (bracket.containsKey(c)) {
+                if (checkList.isEmpty() || checkList.pop() != bracket.get(c)) {
+                    return false;
+                }
+            }
+        }
+        return checkList.isEmpty();
+    }
+
 }
